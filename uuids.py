5d0af0d75c1c5826b45e619474f6a04999206a3c
Mike Luvin <michaelluvin2022@u.northwestern.edu>
2022-05-18 00:11:13 -0500

    create reformatted csv

--- uuids.py
+++ uuids.py
@@ -8,7 +8,7 @@ def load_csv_to_dict(filename):
             uuid_, name = row['uuid'].strip(), row['name'].strip()
             if not name:
                 # some rows are formatted weirdly
-                uuid_, name = uuid_.strip().split(" ", maxsplit=1)
+                uuid_, name = uuid_.strip().split(' ', maxsplit=1)
 
             result_dict[uuid_] = { 
                 'allocation_type': row['allocation_type'].strip(),
@@ -17,5 +17,24 @@ def load_csv_to_dict(filename):
 
     return result_dict
 
-if __name__ == "__main__":
-    print(load_csv_to_dict('uuid_data.csv')['0xFECC'])
+def write_dict_to_csv(data_dict):
+    '''
+    Takes the output from `load_csv_to_dict` and writes it to
+    a csv with the headers `['allocation_type', 'uuid', 'name']`
+    '''
+    with open('reformatted_uuids.csv', 'w', newline='') as csvfile:
+        fieldnames = ['allocation_type', 'uuid', 'name']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for uuid_, info_dict in data_dict.items():
+            row = {
+                'uuid': uuid_, 
+                **info_dict
+            }
+            writer.writerow(row)
+
+
+if __name__ == '__main__':
+    loaded_data_dict = load_csv_to_dict('uuid_data.csv')
+    write_dict_to_csv(loaded_data_dict)
