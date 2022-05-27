dcf00901995bb948c991b6d30d69066a47ad0d49
Mike Luvin <michaelluvin2022@u.northwestern.edu>
2022-05-27 17:55:15 -0500

    change C# script to read csv file

--- csharp_mysql/Program.cs
+++ csharp_mysql/Program.cs
@@ -1,20 +1,17 @@
-﻿using MySql.Data.MySqlClient;
+﻿using System.IO;
+// path to the csv file
+string path = "one.csv";
 
-string cs = @"server=localhost;userid=root;password=password;database=demo";
-
-using var con = new MySqlConnection(cs);
-con.Open();
-
-Console.WriteLine($"MySQL version : {con.ServerVersion}");
-
-// basic SELECT statement
-var query = "SELECT * FROM two;";
-var cmd = new MySqlCommand(query, con);
-var reader = cmd.ExecuteReader();
-// I don't have any data to test this on rn, but it should work?
-// see https://dev.mysql.com/doc/connector-net/en/connector-net-tutorials-sql-command.html
-while (reader.Read()) {
-    Console.WriteLine(reader.ToString());
+string[] lines = System.IO.File.ReadAllLines(path);
+foreach(string line in lines)
+{
+    string[] columns = line.Split(',');
+    for (int i = 0 ; i < columns.Length; i++) {
+        // write blank entries as null for rn
+        if (columns[i] == "") {
+            columns[i] = "null";
+        }
+        Console.Write(columns[i] + " ");
+    }
+    Console.Write("\n");
 }
-con.Close();
-// Console.WriteLine($"{result}");
